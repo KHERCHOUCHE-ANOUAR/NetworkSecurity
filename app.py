f7ac0f7aaeeebb29f6eb4a8e859d5dcb2c2167a9
KHERCHOUCHE-ANOUAR <kherchoucheanouar98@gmail.com>
2025-06-27 14:59:05 +0200

    add a dockerfile and a workflow

--- app.py
+++ app.py
@@ -75,7 +75,11 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
         print(df['predicted_column'])
         #df['predicted_column'].replace(-1, 0)
         #return df.to_json()
-        df.to_csv('prediction_output/output.csv')
+        if os.path.exists('prediction_output'):
+            os.remove('prediction_output/output.csv')
+        else:
+            os.makedirs('prediction_output')
+            df.to_csv('prediction_output/output.csv')
         table_html = df.to_html(classes='table table-striped')
         #print(table_html)
         return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
